156c58a8e38639338a32b0d21cd587cf13ac59ff
Franck Rodolphe RANAIVO-HARISOA <franck-rodolphe.ranaivo-harisoa@externe.grdf.fr>
2022-08-30 17:20:02 +0200

    Ajout fonts

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./Navigation";
+
+export default function Header() {
+//  const [count, setCount] = useState(0)
+
+    return (
+        <div className="Header">
+            <h1 className="title">Franck Ranaivo-Harisoa</h1>
+            <Navigation />
+        </div>
+    )
+}
+
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+export default function Navigation() {
+
+    return (
+        <nav>
+            <ul>
+                <li><a href="#Accueil">Accueil</a></li>
+                <li><a href="#Apropos">À propos</a></li>
+                <li><a href="#Contact">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
